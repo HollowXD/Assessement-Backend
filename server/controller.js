6dade292347b3334a48dd6fe766635750161aedc
Caleb Holdaway <cdholdaway@gmail.com>
2023-03-02 20:58:16 -0700

    fortune done

---
+++ server/controller.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["Chance favors those in motion.", "Bide your time, for success is near.", "Every flower blooms in its own sweet time.", "Don't just think, act!", "Smile when you are ready."];
+      
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
+
+}
